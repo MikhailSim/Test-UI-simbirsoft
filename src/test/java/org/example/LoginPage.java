6152f155371fcc82b182e4cfe6feb27e8b40f82f
MikhailSim <68155383+MikhailSim@users.noreply.github.com>
2022-08-18 23:46:41 +0400

    Add files via upload

---
+++ src/test/java/org/example/LoginPage.java
@@ -0,0 +1,89 @@
+package org.example;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+public class LoginPage {
+    public WebDriver driver;
+    public LoginPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver; }
+
+    @FindBy(xpath = "//*[contains(@id, 'passp-field-login')]")
+    private WebElement loginField;
+
+    @FindBy(xpath = "//*[contains(@id, 'passp:sign-in')]")
+    private WebElement loginBtn;
+
+    @FindBy(xpath = "//*[contains(@id, 'passp-field-passwd')]")
+    private WebElement passwdField;
+
+    @FindBy(xpath = "//*[contains(@id, 'passp:sign-in')]")
+    private WebElement passwdBtn;
+
+    @FindBy(xpath = "/html/body/div[1]/div/div[2]/div[1]/header/div[2]/div[2]/div/div/a[1]/div")
+    private WebElement menuBtn;
+
+    @FindBy(xpath = "/html/body/div/div/div[2]/div[1]/header/div[2]/div[2]/div/div/div/ul/ul/li[3]/a/span")
+    private WebElement driveBtn;
+
+    @FindBy(xpath = "/html/body/div[1]/div/div/div[3]/div[1]/div[1]/div/div/span[2]/button")
+    private WebElement createBtn;
+
+    @FindBy(xpath = "/html/body/div[1]/div/div/div[3]/div[1]/div[3]/div/button[1]/span[1]")
+    private WebElement folderBtn;
+
+    @FindBy(xpath = "/html/body/div[3]/div[2]/div/div/div/div/div/div[1]/div/form/span/input")
+    private WebElement itemField;
+
+    @FindBy(xpath = "/html/body/div[3]/div[2]/div/div/div/div/div/div[2]/button")
+    private WebElement saveBtn;
+
+    @FindBy(xpath = "/html/body/div[1]/div/div/div[3]/div[2]/div[1]/div/div/div[3]/div/div[1]")
+    private WebElement clickBtn;
+
+    @FindBy(xpath = "/html/body/div[1]/div/div/div[3]/div[1]/div[1]/div/div/span[2]/button")
+    private WebElement newBtn;
+
+    @FindBy(xpath = "/html/body/div[1]/div/div/div[3]/div[1]/div[3]/div/button[2]")
+    private WebElement txtBtn;
+
+    @FindBy(xpath = "/html/body/div[3]/div[2]/div/div/div/div/div/div[1]/div/form/span/input")
+    private WebElement txtField;
+
+    @FindBy(xpath = "/html/body/div[3]/div[2]/div/div/div/div/div/div[2]/button")
+    private WebElement newtxtBtn;
+
+
+    public void inputLogin(String login) {
+        loginField.sendKeys("t.simbirsoft@yandex.ru"); }
+    public void inputPasswd(String passwd) {
+        passwdField.sendKeys("Qwerty_123456+"); }
+    public void clickLoginBtn() {
+        loginBtn.click(); }
+    public void clickPasswdBtn() {
+        passwdBtn.click(); }
+    public void clickMenuBtn() {
+        menuBtn.click(); }
+    public void clickDriveBtn() {
+        driveBtn.click(); }
+    public void clickCreateBtn() {
+        createBtn.click(); }
+    public void clickFolderBtn() {
+        folderBtn.click(); }
+    public void inputItem(String item) {
+        itemField.sendKeys(" Test QA"); }
+    public void clickSaveBtn() {
+        saveBtn.click(); }
+    public void clickClickBtn() {
+        clickBtn.click(); }
+    public void clickNewBtn() {
+        newBtn.click(); }
+    public void clickTxtBtn() {
+        txtBtn.click(); }
+    public void inputTxt(String login) {
+        txtField.sendKeys("New txt file"); }
+    public void clickNewtxtBtn() {
+        newtxtBtn.click(); }
+}
---
+++ src/test/java/org/example/PostTest.java
@@ -0,0 +1,60 @@
+package org.example;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+public class PostTest {
+    public static LoginPage loginPage;
+    public static WebDriver driver;
+
+    @BeforeClass
+    public static void setup() {
+        System.setProperty("webdriver.chrome.driver", ConfProperties.getProperty("chromedriver"));
+        driver = new ChromeDriver();
+        loginPage = new LoginPage(driver);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.get(ConfProperties.getProperty("loginpage")); }
+
+    @Test
+    public void postTest() throws InterruptedException {
+        loginPage.inputLogin(ConfProperties.getProperty("t.simbirsoft@yandex.ru"));
+        loginPage.clickLoginBtn();
+        loginPage.inputPasswd(ConfProperties.getProperty("Qwerty_123456+"));
+        loginPage.clickPasswdBtn();
+        loginPage.clickMenuBtn();
+        loginPage.clickDriveBtn();
+        loginPage.clickCreateBtn();
+        loginPage.clickFolderBtn();
+        loginPage.inputItem(ConfProperties.getProperty(" Test QA"));
+        loginPage.clickSaveBtn();
+        Thread.sleep(2000);
+        loginPage.clickClickBtn();
+        WebElement myElement = driver.findElement(By.xpath("/html/body/div[1]/div/div/div[3]/div[2]/div[1]/div/div/div[3]/div/div[1]"));
+        myElement.sendKeys(Keys.RETURN);
+        loginPage.clickNewBtn();
+        loginPage.clickTxtBtn();
+        loginPage.inputTxt(ConfProperties.getProperty("New txt file"));
+        loginPage.clickNewtxtBtn();
+
+        String first = driver.getWindowHandle();
+        Set <String> set = driver.getWindowHandles();
+        set.remove(first);
+        assert set.size()==1;
+        driver.switchTo().window(set.toArray(new String[0])[0]);
+        driver.close();
+        driver.switchTo().window(first);
+
+        List<WebElement> Name = driver.findElements(By.xpath("/html/body/div[1]/div/div/div[3]/div[2]/div[1]/div/div/div[3]/div/div[1]"));
+        int name = Name.size();
+        System.out.println("Find files: " + name);
+
+    } }
